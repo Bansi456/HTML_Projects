4e492fc842f013c25cdff916586d6c94d0e383da
07NovMWF-3-30-Bansi <bansikhanpara560@gmail.com>
2023-02-25 19:47:38 +0530

    form

---
+++ Formvalidation/script.js
@@ -0,0 +1,9 @@
+function validateform(){
+    var name=document.contactform.name.value;
+    var email=document.contactform.email.value;
+    var mobile=document.contactform.mobile.value;
+    var country=document.contactform.country.value;
+    var gender=document.contactform.gender.value;
+    var hobbies=[];
+    var checkboxes=document.getElementsByName("hobbies[]");
+}
